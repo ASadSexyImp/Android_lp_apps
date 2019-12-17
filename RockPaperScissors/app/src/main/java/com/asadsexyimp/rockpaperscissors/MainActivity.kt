e895e1f2fcfe2675b7c6d1b577b4d62a678da910
ASadSexyImp <masakazuplus@gmail.com>
2019-12-17 21:03:22 +0900

    finish fight algoryzm

--- RockPaperScissors/app/src/main/java/com/asadsexyimp/rockpaperscissors/MainActivity.kt
+++ RockPaperScissors/app/src/main/java/com/asadsexyimp/rockpaperscissors/MainActivity.kt
@@ -1,30 +1,40 @@
 package com.asadsexyimp.rockpaperscissors
 
-import android.graphics.drawable.Drawable
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
-    val player: Player = Player(name="Player")
-    val cpu: Player = Player(name="CPU")
+    var player: Player = Player(name="Player")
+    var cpu: Player = Player(name="CPU")
+    var count: Vector3 = Vector3(0,0,0)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+
         imageView.setOnClickListener {
             player.rock()
             cpu.randomnum()
+            fight(player, cpu)
         }
         imageView2.setOnClickListener {
             player.scissors()
             cpu.randomnum()
+            fight(player, cpu)
         }
         imageView.setOnClickListener {
             player.paper()
             cpu.randomnum()
+            fight(player, cpu)
         }
 
         when (player.status) {
@@ -41,6 +51,34 @@ class MainActivity : AppCompatActivity() {
             else -> imageView4.setImageResource(R.drawable.goo2)
         }
 
+        if(count.x == 5){
+            count = Vector3(0,0,0)
+        }
+
+    }
+
+    fun fight(player: Player, cpu: Player){
+        when {
+            player.status == cpu.status ->  {
+                textView.text = "ひきわけ！！！"
+                count + Vector3(0,0,1)
+            }
+            (player.status == "Rock" && cpu.status == "Scissors") || (player.status == "Scissors" && cpu.status == "Paper") || (player.status == "Paper" && cpu.status == "Rock") -> {
+                textView.text = "かち！！！"
+                count + Vector3(1,0,0)
+            }
+            else -> {
+                textView.text = "まけ！！！"
+                count + Vector3(0,1,0)
+            }
+        }
+
+    }
+
+    class Vector3(val x: Int, val y: Int, val z: Int) {
+        operator fun plus(vector: Vector3): Vector3 {
+            return Vector3(x + vector.x, y + vector.y, z + vector.z)
+        }
     }
 
 
